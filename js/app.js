be79991fd7705ff0f026097a6948695c524f517b
Amreal-Lamanera <pieraccini.francesco@gmail.com>
2022-08-05 16:41:07 +0200

    add content

--- js/app.js
+++ js/app.js
@@ -66,9 +66,10 @@ const posts = [
 
 // PRENDO DAL DOM L'ELEMNTO A CUI AGGANCERO' I MIEI CONTENUTI
 const container = document.getElementById('container');
-console.log(container);
+// console.log(container);
 // STAMPO NEL DOM TUTTI I POST CONTENUTI NELL'ARRAY POSTS
 getPosts();
+console.log(posts[0].likes);
 
 function getPosts() {
     posts.forEach(element => {
@@ -76,33 +77,40 @@ function getPosts() {
         const post = document.createElement('div');
         post.className = 'post';
 
-        {// HEADER
+        {   // HEADER
             //creo div class post header
             const postH = document.createElement('div');
             postH.className = 'post__header';
+
             //creo div class post meta
             const postM = document.createElement('div');
             postM.className = 'post-meta';
+
             //creo div class post meta icon
             const postMI = document.createElement('div');
             postMI.className = 'post-meta__icon';
+
             // creo img class profile pc con src e alt presi dall'array posts
             let img = getImg(element);
             // lo appendo a postMI
             postMI.append(img);
+
             // creo div post meta data
             const postMD = document.createElement('div');
             postMD.className = 'post-meta__data';
+
             // creo div contenente l'autore
             const postMA = document.createElement('div');
             postMA.className = 'post-meta__author';
             postMA.innerHTML = element.author.name;
+
             // creo div contenente quando è stato fatto il post
             const postMT = document.createElement('div');
             postMT.className = 'post-meta__time';
             postMT.innerHTML = calcWhen(element.created);
             postMD.append(postMA);
             postMD.append(postMT);
+
             // appendo la struttura
             postM.append(postMI);
             postM.append(postMD);
@@ -110,7 +118,7 @@ function getPosts() {
             post.append(postH);
         }
 
-        {// BODY
+        {   // BODY
             const postT = document.createElement('div');
             postT.className = 'post__text';
             postT.innerHTML = element.content;
@@ -118,10 +126,71 @@ function getPosts() {
 
             const postI = document.createElement('div');
             postI.className = 'post__image';
+
             const postImg = document.createElement('img');
             postImg.src = element.media;
             postI.append(postImg);
             post.append(postI);
+        }
+
+        {   // FOOTER
+            const postF = document.createElement('div');
+            postF.className = 'post__footer';
+
+            const likes = document.createElement('div');
+            likes.classList.add('likes', 'js-likes')
+
+            const cta = document.createElement('div');
+            cta.className = 'likes__cta';
+
+            const a = document.createElement('a');
+            a.classList.add('like-button', 'js-like-button')
+            a.href = '#';
+            a.dataset.postid = element.id;
+            // console.log(a);
+
+            const i = document.createElement('i');
+            i.classList.add("like-button__icon", "fas", "fa-thumbs-up");
+            i.ariaHidden = 'true';
+
+            const likeBtn = document.createElement('span');
+            likeBtn.className = 'like-button__label';
+            likeBtn.innerHTML = 'Mi Piace';
+            let liked = false;
+
+            const counter = document.createElement('b');
+            counter.className = 'js-likes-counter';
+            counter.id = `like-counter-${element.id}`
+            counter.innerHTML = element.likes;
+
+            cta.addEventListener('click', function () {
+                if (!liked) {
+                    a.classList.add('text-primary');
+                    liked = true;
+                    likesCount(true, element.id);
+                }
+                else {
+                    a.classList.remove('text-primary');
+                    liked = false;
+                    likesCount(false, element.id);
+                }
+            })
+
+            a.append(i);
+            a.append(likeBtn);
+            cta.append(a);
+
+            const counterEl = document.createElement('div');
+            counterEl.className = 'likes__counter';
+            counterEl.innerHTML = 'Piace a '
+            counterEl.append(counter);
+            counterEl.innerHTML += ' persone.'
+
+            likes.append(cta);
+            likes.append(counterEl);
+            postF.append(likes);
+            post.append(postF);
+
         }
 
         container.append(post);
@@ -139,13 +208,13 @@ function calcWhen(postDate) {
     // console.log(month);
     let message = '';
     const diffYear = currentYear - year;
-    console.log(diffYear);
+    // console.log(diffYear);
 
     const diffMonth = currentMonth - month
-    console.log(diffMonth);
+    // console.log(diffMonth);
 
     const diffDays = currentDay - day;
-    console.log(diffDays);
+    // console.log(diffDays);
 
     if (diffYear > 1) {
         message += `${diffYear} anni `;
@@ -183,4 +252,10 @@ function getImg(element) {
 function getFirstLetter(element) {
     [nome, cognome] = element.author.name.split(' ');
     return nome[0] + cognome[0];
+}
+
+function likesCount(bool, id) {
+    const counter = document.getElementById(`like-counter-${id}`)
+    if (bool) counter.innerHTML++;
+    else counter.innerHTML--;
 }
